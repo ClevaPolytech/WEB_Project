99eef5febcca82d426980b872b88ba7bd0b59209
ClevaPolytech <45760899+ClevaPolytech@users.noreply.github.com>
2019-03-30 16:54:34 +0100

    Add files via upload

--- PROJET/jeu.js
+++ PROJET/jeu.js
@@ -5,6 +5,8 @@ window.onload=function(){
     let GameStarted=false;
     let PlayerBoat=[];
     let ordiBoat=[];
+    let ordiCoup=[];
+    let touche=[false,"X"];
     let countCellBoat=0;
 
     let PlayerselectCase; //case selectionné par le joueur
@@ -48,10 +50,13 @@ window.onload=function(){
 
     }
 
-    function caseAdj(obj){
+    function caseAdj(obj,joueur){
+        let tableau;
+        if (joueur == "player") { tableau = PlayerBoat[indexBoat];}
+        else{tableau=lastTour[1];}
 
-        for (let i=0; i<PlayerBoat[indexBoat].length; i++){
-            let cell = PlayerBoat[indexBoat][i];
+        for (let i=0; i<tableau.length; i++){
+            let cell =tableau[i];
             if (cell[0] == obj.id[0] && Number(cell[1])+1 == obj.id[1]&&elemCommun(obj)){
                 return true;
             }
@@ -69,18 +74,56 @@ window.onload=function(){
 
     }
 
-    function PlacerOrdi(){
-        for(let i=0; i<dimBoat.length;i++){
-            let lettre = tab[getRandomInt(10)];
-            let chiffre = getRandomInt(10);
-            
-        }
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
     }
+
+    function PlacerOrdi(){
+        /*for(let i=0; i<dimBoat.length;i++){
+            while(val0-dimBoat[i]<0){
+                let val0 = tab[getRandomInt(10)];
+                }
+                
+                let chiffre = 0;
+                ordiBoat.push([val0 + chiffre]);
+                alert(ordiBoat);
+                for (let j = 0; j < dimBoat[i]; j++) {
+
+            }
+            
+        }*/
+        ordiBoat = ["A3", "B3", "C3", "D3", "E3", "F3", "C6", "C7", "C8", "C9", "G6", "H6", "I6", "I2", "I3"];
+    }
+
+    function ordiIA(){
+        let verif=false;
+        let coup=aleatoire();
+        /*if(lastTour[0]==false){
+            while (verif == false){
+                coup = aleatoire();
+                verif=caseAdj(coup,"ordi");
+            }
+            checkHit(coup,"ordi");
+        }
+        else{*/
+            let ordiCell=document.getElementById(coup+"P");
+            checkHit(ordiCell,"ordi");
+        //}
+    }
+
+    function aleatoire(){
+        let val0 = tab[getRandomInt(10)];
+        let val1 = getRandomInt(10);
+        while(ordiCoup.indexOf(val0+val1)!==-1){
+            val0 = tab[getRandomInt(10)];
+            val1 = getRandomInt(10);}
+        return val0+val1;
+        }
 
     //appeler au début pour que le joueur place ses bateaux
     function PlacerBateau(obj){
         if (PlayerBoat[indexBoat]!=null){
-            if(caseAdj(obj)){
+            if(caseAdj(obj,"player")){
                 PlayerBoat[indexBoat].push(obj.id);
                 BoatCase(obj.id);
             }
@@ -95,6 +138,7 @@ window.onload=function(){
             countCellBoat = 0;
             if(dimBoat[indexBoat]==null){
                 GameStarted=true;
+                main();
                 alert(PlayerBoat);
             }
             else{
@@ -112,27 +156,51 @@ window.onload=function(){
     }
 
     //vérifie si touché ou non
-    function checkHit(){
-
+    function checkHit(obj,joueur){
+        if(ordiBoat.indexOf(obj.id)<0&&joueur=="player"){
+            BadCase(obj.id);
+            
+        }
+        
+        else if (PlayerBoat[3].indexOf(obj.id) < 0 &&PlayerBoat[2].indexOf(obj.id) < 0 &&PlayerBoat[1].indexOf(obj.id) < 0 &&PlayerBoat[0].indexOf(obj.id) < 0 && joueur == "ordi") {
+            alert(obj.id);
+            BadCase(obj.id);
+            touche[0] = false;
+            ordiCoup.push(obj.id);
+        }
+        else{
+            GoodCase(obj.id);
+            if(joueur=="player"){ordiBoat.splice(ordiBoat.indexOf(obj.id),1);}
+            else { PlayerBoat.splice(PlayerBoat.indexOf(obj.id), 1);
+                touche[1]=obj.id;
+                touche[0]=true;
+                ordiCoup.push(obj.id);}
+        }
     }
-
+    PlacerOrdi();
     if(!GameStarted){
         let restart=document.getElementById("restart");
         restart.onclick=replacer;
-    }
-
     //crée un evenement de clic pour chaque case
-    let board1=document.getElementsByClassName("cell");
-    for (n = 0; n < 100; n++) {
+        let board1=document.getElementsByClassName("cell");
+        for (n = 0; n < 100; n++) {
             board1[n].onclick=function(){
-                if(GameStarted){
-                    
+            if(GameStarted==false){
+                PlacerBateau(this);
                 }
-                else{
-                    PlacerBateau(this);}
+                
             }
-            
-        
+        }
     }
 
+    function main(){
+        let board2 = document.getElementsByClassName("cell");
+        for (n = 100; n < 200; n++) {
+            board2[n].onclick = function () {
+            alert(this.id);
+            checkHit(this,"player");
+            ordiIA();
+            }
+        }
+    }
 }
